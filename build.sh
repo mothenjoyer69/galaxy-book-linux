ecb8a885bdc91b60b8dbed1484c19e81fb1ade57
davis <97616889+mothenjoyer69@users.noreply.github.com>
2022-09-13 14:57:08 +1000

    build.sh: complete rewrite

--- build.sh
+++ build.sh
@@ -1,123 +1,56 @@
 #!/bin/bash
-KERNEL=https://gitlab.com/sdm845-mainline/linux
-DTB=sdm850-samsung-w737.dtb 
-DESTDIR=$1
-EFI=$11
-ROOT=$12
-export ARCH=arm64
-export CROSS_COMPILE=aarch64-linux-gnu-
-
-
-user_menu() {
-    echo -ne "
-Choose an option:
-1) Do everything
-2) Build kernel only
-3) Prepare drive only
-4) Install to device
-5) Exit
-Choose an option:  "
-    read -r input
-    case $input in
-    1)
-        prepare_dest $1
-        build_kernel
-        rootfs_install $1
-        grub_install
-        umount_all $1
-        exit 0
-        ;;
-    2)
-        prepare_dest $1
-        build_kernel $1
-        cd linux   
-        make INSTALL_MOD_PATH=/mnt modules_install
-        exit 0
-        ;;
-    3)
-        prepare_dest $1
-        umount_all $1
-        exit 0
-        ;;
-    4)
-        echo "Not implemented"
-        #todo: add this. first step, find out what the hell is causing the display to blank.
-        user_menu
-        ;;
-    5) exit 0
-        ;;
-    *)
-        echo "Invalid selection"
-        user_menu
-        ;;
+set -e
+BR2_DIR="buildroot"
+BUILD_CROSS="CROSS_COMPILE=aarch64-linux-gnu- ARCH=arm64"
+OUT_DIR="out"
+CMD=$1$2
+CONFIG=samsung-w737_defconfig
+#functions
+flash_usb () {
+    read -p "target device " USB
+    echo "you entered $USB"
+    #make sure you aren't about to nuke your primary drive or anything
+    while true; do
+        read -p "is $USB the correct device? " yn
+        case $yn in
+            [Yy]* ) echo "yes"; break;;
+            [Nn]* ) flash_usb; break;;
+            * ) echo "yes or no";;
+        esac
+    done
+    #create partition, only EFI for now as initramfs, bootloader, kernel, and dtb are all in the EFI partition.
+    parted -s $USB mklabel gpt
+    parted -s $USB mkpart "EFI" fat32 1MiB 261MiB
+    parted -s $USB set 1 esp on
+    mkfs.vfat /dev/sda1
+    mount $USB'1' /mnt
+    cp -r $OUT_DIR/* /mnt
+    cp -r extras/grub.cfg /mnt/EFI/BOOT/grub.cfg
+    umount $USB'1'
+    echo "now boot it" && exit
+}
+#download latest linux and buildroot tarball
+if [[ ! -d 'buildroot' ]]
+    then echo "looking for buildroot directory" && mkdir $BR2_DIR && wget https://buildroot.org/downloads/buildroot-2022.05.1.tar.gz -o buildroot.tar.gz && bsdtar xzf buildroot.tar.gz -C $BR2_DIR/
+    else echo "buildroot exists"
+fi
+#check if user wants to skip the rootfs, or wants to prepare for a clean build
+case $CMD in
+    -norootfs ) echo "not building rootfs" ;;
+    -clean ) echo "cleaning directory" cd $BR2_DIR && make clean && cd .. && rm -rf $OUT_DIR && echo "cleaned" && exit;;
+    *) cd $BR2_DIR && make $CONFIG && make BR2_JLEVEL=$(nproc) && cd ..
+esac
+#moving files
+echo "moving files"
+[[ ! -d $OUT_DIR ]] && mkdir $OUT_DIR/
+export OUTPUT_FILES=$BR2_DIR/output/images/
+for i in $OUTPUT_FILES/Image $OUTPUT_FILES/efi-part/EFI $OUTPUT_FILES/rootfs.cpio $OUTPUT_FILES/sdm850-samsung-w737.dtb; do cp -r $i $OUT_DIR; done
+echo "$PWD/$OUT_DIR contains built files"
+while true; do
+    read -p "install to usb? " yn
+    case $yn in
+        [Yy]* ) flash_usb; break;;
+        [Nn]* ) echo "skipping flash, done" && exit;;
+        * ) echo "please answer yes or no";;
     esac
-}
-
-prepare_dest () {
-    lsblk #check partitions are correct and that you did in fact NOT just nuke your storage drive
-    sleep 10
-    #dd if=/dev/zero of=$DESTDIR bs=4M
-    parted $DESTDIR mklabel gpt
-    parted $DESTDIR mkpart fat32 1MiB 301MiB
-    parted $DESTDIR set 1 boot on
-    parted $DESTDIR mkpart ext4 301MiB 100%
-    mkfs.fat -F 32 $EFI
-    mkfs.ext4 $ROOT
-    mount $ROOT /mnt
-    mkdir /mnt/boot
-    mount $EFI /mnt/boot
-    
-}
-
-build_kernel () {
-    if [[ $2 = "-skip-download" ]]; 
-        then 
-            echo "Not downloading kernel git"
-        else
-            git clone $KERNEL
-    fi
-    cp linux/arch/arm64/configs/sdm845.config linux/.config
-    cd linux
-    make defconfig sdm845.config 
-    make menuconfig
-    make -j$(nproc)
-    make dtbs
-    make install INSTALL_PATH=/mnt/boot
-    cp arch/arm64/boot/dts/qcom/$DTB .
-    cd ..
-}
-
-rootfs_install () { 
-    debootstrap --arch="arm64" "testing" "/mnt"  https://deb.debian.org/debian/
-    #ugly way to prepare for chroot
-    mount --bind /dev /mnt/dev &&
-    mount --bind /dev/pts /mnt/dev/pts &&
-    mount --bind /proc /mnt/proc &&
-    mount --bind /sys /mnt/sys
-    mkdir -p /mnt/lib/firmware/qcom/samsung/w737
-    cp -r firmware/system/* /mnt/lib/firmware/qcom/samsung/w737 #lmao firmware is probably signed so uh gotta figure out how to get this done easily for other devices
-    cd linux
-    make INSTALL_MOD_PATH=/mnt modules_install
-    chroot /mnt apt update
-    chroot /mnt apt install 
-}
-
-grub_install () {
-    chroot /mnt apt install grub-efi-arm64-bin grub2-common
-    chroot /mnt grub-install --target=arm64-efi --efi-directory /boot --bootloader-id=GRUB
-    chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
-    cp $DTB /mnt/boot
-    cp $DTB /mnt/boot/grub
-}
-
-umount_all () {
-    umount /mnt/dev/pts
-    umount /mnt/dev
-    umount /mnt/proc
-    umount /mnt/sys
-    umount $EFI
-    umount $ROOT
-    
-}
-
-user_menu
+done
