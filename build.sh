25fd2aa9491754475d95aa29302992fde8f1ce1e
davis <97616889+mothenjoyer69@users.noreply.github.com>
2022-09-14 18:36:41 +1000

    build.sh: update

--- build.sh
+++ build.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -e
+KERN_DTB="qcom/sdm850-samsung-w737.dtb"
+KERN_DIR="linux"
 BR2_DIR="buildroot"
 BUILD_CROSS="CROSS_COMPILE=aarch64-linux-gnu- ARCH=arm64"
 OUT_DIR="out"
+ROOTFS="rootfs.cpio"
 CMD=$1$2
 CONFIG=samsung-w737_defconfig
 #functions
 flash_usb () {
     read -p "target device " USB
     echo "you entered $USB"
-    #make sure you aren't about to nuke your primary drive or anything
     while true; do
         read -p "is $USB the correct device? " yn
         case $yn in
@@ -18,27 +20,26 @@ flash_usb () {
             * ) echo "yes or no";;
         esac
     done
-    #create partition, only EFI for now as initramfs, bootloader, kernel, and dtb are all in the EFI partition.
     parted -s $USB mklabel gpt
-    parted -s $USB mkpart "EFI" fat32 1MiB 261MiB
+    parted -s $USB mkpart "EFI" fat32 1MiB 977MiB
     parted -s $USB set 1 esp on
     mkfs.vfat /dev/sda1
     mount $USB'1' /mnt
     cp -r $OUT_DIR/* /mnt
     cp -r extras/grub.cfg /mnt/EFI/BOOT/grub.cfg
     umount $USB'1'
-    echo "now boot it" && exit
+    echo "lmao maybe this will work" && exit
 }
 #download latest linux and buildroot tarball
 if [[ ! -d 'buildroot' ]]
     then echo "looking for buildroot directory" && mkdir $BR2_DIR && wget https://buildroot.org/downloads/buildroot-2022.05.1.tar.gz -o buildroot.tar.gz && bsdtar xzf buildroot.tar.gz -C $BR2_DIR/
     else echo "buildroot exists"
 fi
-#check if user wants to skip the rootfs, or wants to prepare for a clean build
+#check if user wants to skip the kernel, rootfs, or wants to prepare for a clean build
 case $CMD in
     -norootfs ) echo "not building rootfs" ;;
-    -clean ) echo "cleaning directory" cd $BR2_DIR && make clean && cd .. && rm -rf $OUT_DIR && echo "cleaned" && exit;;
-    *) cp extras/$CONFIG $BR2_DIR && cd $BR2_DIR && make $CONFIG && make BR2_JLEVEL=$(nproc) && cd ..
+    -clean ) echo "cleaning directory" && cd $KERN_DIR && make clean && cd .. && cd $BR2_DIR && make clean && cd .. && rm -rf $OUT_DIR && echo "cleaned" && exit;;
+    *) cd $BR2_DIR && make $CONFIG && make BR2_JLEVEL=$(nproc) && cd ..
 esac
 #moving files
 echo "moving files"
@@ -46,6 +47,8 @@ echo "moving files"
 export OUTPUT_FILES=$BR2_DIR/output/images/
 for i in $OUTPUT_FILES/Image $OUTPUT_FILES/efi-part/EFI $OUTPUT_FILES/rootfs.cpio $OUTPUT_FILES/sdm850-samsung-w737.dtb; do cp -r $i $OUT_DIR; done
 echo "$PWD/$OUT_DIR contains built files"
+#partition drive
+#fdisk $USB #add later. 3 partitions, 1 @ sector 34-1048609, 2 @ 1048910-2621473, 3 @ 2621474 (fill rest of drive)
 while true; do
     read -p "install to usb? " yn
     case $yn in
