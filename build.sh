216364a72c26af0eb05fd17498b3166f7146d847
mothenjoyer69 <no112@protonmail.com>
2022-09-15 14:41:21 +1000

    build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 KERN_DTB="qcom/sdm850-samsung-w737.dtb"
 KERN_DIR="linux"
-BR2_DIR="buildroot"
+BR2_DIR="buildroot-2022.05.1"
 BUILD_CROSS="CROSS_COMPILE=aarch64-linux-gnu- ARCH=arm64"
 OUT_DIR="out"
 ROOTFS="rootfs.cpio"
@@ -32,23 +32,22 @@ flash_usb () {
 }
 #download latest linux and buildroot tarball
 if [[ ! -d 'buildroot' ]]
-    then echo "looking for buildroot directory" && mkdir $BR2_DIR && wget https://buildroot.org/downloads/buildroot-2022.05.1.tar.gz -o buildroot.tar.gz && bsdtar xzf buildroot.tar.gz -C $BR2_DIR/
+    then echo "downloading buildroot" && echo "mkdir $BR2_DIR" && wget https://buildroot.org/downloads/buildroot-2022.05.1.tar.gz && bsdtar xzf buildroot-2022.05.1.tar.gz && cp extras/$CONFIG $BR2_DIR/configs/$CONFIG
     else echo "buildroot exists"
 fi
 #check if user wants to skip the kernel, rootfs, or wants to prepare for a clean build
 case $CMD in
     -norootfs ) echo "not building rootfs" ;;
     -clean ) echo "cleaning directory" && cd $KERN_DIR && make clean && cd .. && cd $BR2_DIR && make clean && cd .. && rm -rf $OUT_DIR && echo "cleaned" && exit;;
-    *) cd $BR2_DIR && make $CONFIG && make BR2_JLEVEL=$(nproc) && cd ..
+    *) cd $BR2_DIR && make $CONFIG && make BR2_JLEVEL=$(nproc) 2>&1|tee build.log && cd ..
 esac
 #moving files
 echo "moving files"
 [[ ! -d $OUT_DIR ]] && mkdir $OUT_DIR/
-export OUTPUT_FILES=$BR2_DIR/output/images/
+export OUTPUT_FILES=$BR2_DIR/output/images
 for i in $OUTPUT_FILES/Image $OUTPUT_FILES/efi-part/EFI $OUTPUT_FILES/rootfs.cpio $OUTPUT_FILES/sdm850-samsung-w737.dtb; do cp -r $i $OUT_DIR; done
 echo "$PWD/$OUT_DIR contains built files"
 #partition drive
-#fdisk $USB #add later. 3 partitions, 1 @ sector 34-1048609, 2 @ 1048910-2621473, 3 @ 2621474 (fill rest of drive)
 while true; do
     read -p "install to usb? " yn
     case $yn in
